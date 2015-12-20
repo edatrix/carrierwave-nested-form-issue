6a986f0314915c0a60649809cf161f36250033e1
edatrix <lespath@gmail.com>
2015-12-20 09:57:54 -0700

    create before action to find elf, makes routing nicer

--- app/controllers/elves_controller.rb
+++ app/controllers/elves_controller.rb
@@ -1,4 +1,5 @@
 class ElvesController < ApplicationController
+  before_action :find_elf, :only => [:show, :edit, :update, :destroy]
 
   def index
     @elves = Elf.all
@@ -11,19 +12,38 @@ class ElvesController < ApplicationController
 
   def create
     @elf = Elf.new(elf_params)
-    @elf.save
+
+    respond_to do |format|
+      if @elf.save
+        format.html { redirect_to elves_path, :notice => "#{@elf.name} was successfully created." }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
-    @elf = Elf.find(params[:id])
   end
 
   def update
-    @elf = Elf.find(params[:id])
-    render :create
+    respond_to do |format|
+      if @elf.update(elf_params)
+        format.html { redirect_to elves_path, :notice => "#{@elf.name} was successfully updated." }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @elf.destroy
   end
 
   private
+
+  def find_elf
+    @elf = Elf.find(params[:id])
+  end
 
   def elf_params
     params.require(:elf).permit([:name, :toys_attributes => [:id, :name, :image, :remove_image, :_destroy]])
